35c05d7795c6c6f02ce17cfbc6887067021fddf1
matticry <matticry1@gmail.com>
2025-05-22 14:54:26 -0500

    updates

--- APIDiscovery/obj/Debug/net8.0/APIDiscovery.AssemblyInfo.cs
+++ APIDiscovery/obj/Debug/net8.0/APIDiscovery.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIDiscovery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1141510798491c8f23e13adb4b0c6d1a5bf83f28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d183f67cb651f7ed985643dfdbef1c447e1ff294")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIDiscovery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIDiscovery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
